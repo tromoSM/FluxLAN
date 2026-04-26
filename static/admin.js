02ac0ca4423f11cf3e924c13b4d46248a008b6f7
tromoSM <tromosm7@gmail.com>
2026-04-26 09:58:37 +0530

    Refactor stream fetching to use socket.io

--- static/admin.js
+++ static/admin.js
@@ -1,18 +1,7 @@
 window.addEventListener('DOMContentLoaded',function(){
-    setInterval(()=>{
-     fetch('/stream')
-     .then(response=> response.json())
-     .then(re=>{
-        document.querySelector(`[stream='src']`).src=re.rec
-        if(re.ori=='up'){
-            document.querySelector(`[stream='src']`).style.transform='rotate(0deg)'
-        }
-        else{
-            document.querySelector(`[stream='src']`).style.transform='rotate(90deg)'
-        }
-     })
-     .catch(err=>{
-      console.error('Error',err)
-     })
-    },100)
+    const S=io()
+    S.on('Stream',function(stream){
+        document.querySelector(`[stream='src']`).src=stream.rec
+        document.querySelector(`[stream='src']`).style.transform=`rotate(${stream.ori}deg)`
+    })
 })
