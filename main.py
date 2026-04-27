76b81f536b59c8ccd6dff1bd9bff36cf23a4a497
tromoSM <tromosm7@gmail.com>
2026-04-27 20:21:20 +0530

    Add user join and leave event handling

--- main.py
+++ main.py
@@ -2,16 +2,58 @@ from flask import Flask,request,render_template,abort
 from flask_socketio import SocketIO,emit
 import os
 main=Flask(__name__,template_folder=os.path.join("templates"),static_folder=os.path.join("static"))
-
+MAINUSERSDI={}
+ALLUSERS=[] #keep this
 S=SocketIO(main,cors_allowed_origins="*")
 LASTFrame='__not-found__'
 orientation='up'
+
+# GLOBAL UI FUNCc
+def joinev(user):
+    if user not in ALLUSERS:
+     ALLUSERS.append(user)
+    else:
+       #emtIOerrUSR
+       pass
+    S.emit('adminJOIN',user)      
+
+def leaveev(user):
+    if user in ALLUSERS:
+        ALLUSERS.remove(user)
+    S.emit('adminLEAVE',user)  
+
+def UserError(user,title,description): # DONT USE YET
+   S.emit('notification',{"user":user,"title":title,'description':description}) #change to S.to SID
+#---
 @S.on('Process')
 def mainroute(data):
    global LASTFrame
    if data!=LASTFrame:
     S.emit('Stream',data)
     LASTFrame=data
+
+@S.on("join")
+def ini(dih):
+    MAINUSERSDI[request.sid]=dih['clieUSR']
+    curuser=MAINUSERSDI.get(request.sid,request.sid)
+    print(f"│ User connected : {curuser}")
+    joinev(curuser)
+
+@S.on('disconnect')
+def left():
+    curUser=MAINUSERSDI.get(request.sid,request.sid)
+    print(f"│ User disconnected : {curUser}")
+    leaveev(curUser)      
+    MAINUSERSDI.pop(request.sid,None)
+
+@S.on('CamMinimize')
+def min(user):
+    leaveev(user)
+
+@S.on('CamRevive')
+def revive(user):
+    joinev(user)
+
 @main.route('/')
 def web():
  return render_template('index.html')
