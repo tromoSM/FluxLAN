a69421e86509b8a87551f9b03549cd3df144702a
tromoSM <tromosm7@gmail.com>
2026-04-27 20:18:20 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,9 +1,42 @@
 window.addEventListener('DOMContentLoaded',function(){
 let mainSt={rec:'__not-found__',ori:0}
+let afacingMode
+//loc-aft username prompt
+const username='mainuser'
+if(!localStorage.getItem('facingMode')){
+    localStorage.setItem('facingMode','environment')
+    afacingMode='environment'
+}
+else{
+    afacingMode=localStorage.getItem('facingMode')
+}
 let contx$$RUNNING=false
 let orin=0
 const S=io()
+S.emit('join',{clieUSR:username})
+window.addEventListener('visibilitychange',function(){
+    if(document.hidden){
+        S.emit('CamMinimize',username)
+    }
+    else{
+        S.emit('CamRevive',username)
+    }
+})
+S.on('JSPrefREC',function(pref){
+ if(Object.keys(pref)[0]=='face'){
+  if(afacingMode=='environment'){
+    afacingMode='user'
+    localStorage.setItem('facingMode','user')
+    window.location.reload()
+  }
+  else{
+    afacingMode='environment'
+    localStorage.setItem('facingMode','environment')
+    window.location.reload()
 
+  }
+ }
+})
 document.querySelector(`[chnO="o"]`).addEventListener('click',function(){
 if(orin==270){
    orin=0 
@@ -14,11 +47,11 @@ else{
 })
 async function unRStream(){
 if(!navigator.mediaDevices||navigator.mediaDevices==undefined){
- let mainSt={rec:'__not-allowed__'} 
+  mainSt={rec:'__not-allowed__'} 
 }
 else{
     try{ 
-     let mainRT=await navigator.mediaDevices.getUserMedia({video: {facingMode : "environment"}})
+     let mainRT=await navigator.mediaDevices.getUserMedia({video: {facingMode : afacingMode}})
      let mainVD=document.querySelector('video')
      let mainC=document.querySelector(`[dupSEc='object']`)
      let CanV=mainC.getContext('2d')
@@ -33,6 +66,7 @@ else{
      mainSt={rec:imim,ori:orin}
      S.emit('Process',mainSt)
      },100)
+     
     }
     catch(er){
         console.error(er)
