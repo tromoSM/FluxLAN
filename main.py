ec0d49f7575dcf8bf692ceec6517955ab4876f8a
tromoSM <tromosm7@gmail.com>
2026-04-27 18:48:03 +0530

    Refactor socket handling and add new event listener

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import os
 main=Flask(__name__,template_folder=os.path.join("templates"),static_folder=os.path.join("static"))
 
 S=SocketIO(main,cors_allowed_origins="*")
-
-ECHFRM='__not found__'
+LASTFrame='__not-found__'
 orientation='up'
 @S.on('Process')
 def mainroute(data):
-   S.emit('Stream',data)
+   global LASTFrame
+   if data!=LASTFrame:
+    S.emit('Stream',data)
+    LASTFrame=data
 @main.route('/')
 def web():
  return render_template('index.html')
@@ -19,6 +21,8 @@ def admin():
       abort(403)
       return 'sybau'
    return render_template('tromoSM-admin.html')
-
+@S.on('Pref')
+def mainfunc(pref):
+        S.emit('JSPrefREC',pref)
 if(__name__=="__main__"):
     S.run(main,debug=True,host='0.0.0.0',port='84',ssl_context=('192.168.1.XX.pem', '192.168.1.XX-key.pem'))
