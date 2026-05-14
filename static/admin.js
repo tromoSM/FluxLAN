f8e0442a53041adad5dc1ea8ea23c61c895c0b9d
tromoSM <tromosm7@gmail.com>
2026-05-15 00:50:39 +0530

    Enhance admin.js with link lookup and footer updates
    Added link lookup and updated footer information.

--- static/admin.js
+++ static/admin.js
@@ -14,11 +14,14 @@ window.addEventListener('DOMContentLoaded',function(){
         S.emit('OpenDir',path)
     }
     let UPDATECHKROOT=''
+    let LINKLOOKUP=''
     S.emit('INFO','about',(app)=>{
         window.FluxLAN_version=app.version
         window.FluxLAN_version_release=app.version_release
         window.FluxLAN_release_date=app.date
+        window.FluxLAN_build=app.build
         UPDATECHKROOT=app.updatechk_root
+        LINKLOOKUP=app.link_lookup
     })
     function cammessage(a){
         let full=document.createElement('f')
@@ -64,6 +67,43 @@ window.addEventListener('DOMContentLoaded',function(){
 
         }
 
+    }
+    function refreshLinks(){
+        //add mutationobserver for standalone
+        document.querySelectorAll('[openlink]').forEach(yo=>{
+            yo.addEventListener('click',function(){
+                let temphref=document.createElement('a')
+                temphref.href=yo.getAttribute('openlink')
+                if(yo.hasAttribute('target')){
+                    temphref.target=yo.getAttribute('target')
+                    if(yo.getAttribute('target')=='_blank'){
+                        temphref.rel='noreferrer'
+                    }
+                }
+                temphref.style.display='none'
+                document.body.appendChild(temphref)
+                temphref.click()
+            })
+            if(!yo.hasAttribute('removeIndicator')){
+                let temp=document.createElement('linkindicator')
+                if(yo.hasAttribute('indicatorText')){
+                temp.innerText=yo.getAttribute('indicatorText')
+                }
+                else{temp.innerText=yo.getAttribute('openlink')}
+                yo.addEventListener('mouseenter',async function(){
+                if(temp.hasAttribute('closing')){
+                        temp.removeAttribute('closing')
+                }
+                document.body.appendChild(temp)
+
+                })
+                yo.addEventListener('mouseleave',async function(){
+                    temp.setAttribute('closing','')
+                    await sleep(300)
+                    temp.remove()
+                })
+            }
+        })
     }
     S.on('AdminNotification',function(data){
         notification({title:data.title,body:data.body,icon:data.icon,level:data.level,timeout:data.timeout})
@@ -535,7 +575,7 @@ window.addEventListener('DOMContentLoaded',function(){
                  let infobar=document.createElement('p')
                  infobar.setAttribute('warning','')
                  if(UPDATECHKROOT!=''){
-                    infobar.innerText=`${window.FluxLAN_version} ${window.FluxLAN_release_date} `
+                    infobar.innerText=`${window.FluxLAN_version} ${window.FluxLAN_release_date} (${window.FluxLAN_build})`
                  }
                  else{window.location.reload()}
                  let flexxr=document.createElement('flexrr')
@@ -549,6 +589,10 @@ window.addEventListener('DOMContentLoaded',function(){
                  flexxr.append(chkupdate,flexC)
                  let seperatorS=document.createElement('seperator')
                  seperatorS.setAttribute('small','')
+                 //info
+                 let flexxraa=document.createElement('flexrr')
+                 flexxraa.append()
+                 //footer
                  let flexxra=document.createElement('flexrr')
                  let clearall=document.createElement('button')
                  let shutall=document.createElement('button')
@@ -559,18 +603,41 @@ window.addEventListener('DOMContentLoaded',function(){
                  flexxra.append(clearall,shutall)
                  // add openlog button
                  flexxra.setAttribute('cache','')
+                 let social=document.createElement('flexrr')
+                 social.setAttribute('social','')
+                 let github=document.createElement('button')
+                 github.innerHTML=`<svg viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M56.7937 84.9688C44.4187 83.4688 35.7 74.5625 35.7 63.0313C35.7 58.3438 37.3875 53.2813 40.2 49.9063C38.9812 46.8125 39.1687 40.25 40.575 37.5313C44.325 37.0625 49.3875 39.0313 52.3875 41.75C55.95 40.625 59.7 40.0625 64.2937 40.0625C68.8875 40.0625 72.6375 40.625 76.0125 41.6563C78.9187 39.0313 84.075 37.0625 87.825 37.5313C89.1375 40.0625 89.325 46.625 88.1062 49.8125C91.1062 53.375 92.7 58.1563 92.7 63.0313C92.7 74.5625 83.9812 83.2813 71.4187 84.875C74.6062 86.9375 76.7625 91.4375 76.7625 96.5938L76.7625 106.344C76.7625 109.156 79.1062 110.75 81.9187 109.625C98.8875 103.156 112.2 86.1875 112.2 65.1875C112.2 38.6563 90.6375 17 64.1062 17C37.575 17 16.2 38.6562 16.2 65.1875C16.2 86 29.4187 103.25 47.2312 109.719C49.7625 110.656 52.2 108.969 52.2 106.438L52.2 98.9375C50.8875 99.5 49.2 99.875 47.7 99.875C41.5125 99.875 37.8562 96.5 35.2312 90.2188C34.2 87.6875 33.075 86.1875 30.9187 85.9063C29.7937 85.8125 29.4187 85.3438 29.4187 84.7813C29.4187 83.6563 31.2937 82.8125 33.1687 82.8125C35.8875 82.8125 38.2312 84.5 40.6687 87.9688C42.5437 90.6875 44.5125 91.9063 46.8562 91.9063C49.2 91.9063 50.7 91.0625 52.8562 88.9063C54.45 87.3125 55.6687 85.9063 56.7937 84.9688Z" fill="#8f8f8f"/> </svg>`
+                 github.setAttribute('openlink','https://github.com/tromoSM/FluxLAN')
+                 github.setAttribute('target','_blank')
+                 github.setAttribute('tooltip','open github repo')
+                 social.setAttribute('socialfoot','')
+                 let otherprojx=document.createElement('p')
+                 otherprojx.innerText='Other projects from developer'
+                 otherprojx.setAttribute("target",'_blank')
+                 let feedback=document.createElement('p')
+                 feedback.innerText='feedback or request features'
+                 fetch(LINKLOOKUP).then(n=>
+                     n.json()).then(nf=>{
+                       otherprojx.setAttribute('indicatorText',`${nf.pages.root.split('//')[1]}projects`)
+                       otherprojx.setAttribute('openlink',`${nf.pages.root}?utm_source=fluxlan_inapp_projects_advanced#project`)
+                       feedback.setAttribute('openlink',`${nf.pages.feedback}&utm_source=fluxlan_inapp_feedback_advanced`)
+                       feedback.setAttribute('indicatorText',`${nf.pages.root.split('//')[1]}feedback-or-request-features`)
+                 })
+
+                 social.append(github,otherprojx,feedback)
+
                  let footer=document.createElement('flexrr')
                  let copyright=document.createElement('p')
                  copyright.setAttribute('warning','')
                  copyright.innerHTML='© 2026 tromoSM. Licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0">apache 2.0</a>. FluxLAN is open-source • <a href="https://github.com/tromoSM/FluxLAN">contribute here</a>'
                  footer.append(copyright)
-                 inscroll.append(imim,infobar,flexxr,seperatorS,flexxra,footer)
+                 inscroll.append(imim,infobar,flexxr,seperatorS,flexxraa,flexxra,social,footer)
                  full.appendChild(infull)
                  document.body.append(full)
                  await sleep(200)
                  infull.removeAttribute('closing','')
-                 
-
+                 refreshLinks()
+                 refreshTooltip()
                 chkupdate.addEventListener('click',async function(){
                  fetch(UPDATECHKROOT).then(n=>
                   n.json()).then(async app=>{
@@ -724,5 +791,13 @@ window.addEventListener('DOMContentLoaded',function(){
         }
         refreshTooltip()
        }
+    })
+    document.querySelectorAll('[stream]').forEach(im=>{
+        im.addEventListener('dbclick',function(){
+            if(im.hasAttribute('grid')){
+                im.removeAttribute('grid')
+            }
+            else{im.setAttribute('grid','')}
+        })
     })
 })
