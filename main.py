35e737c3642d490432b88fc278db21d9b5d628b2
tromoSM <tromosm7@gmail.com>
2026-05-21 15:59:40 +0530

    Enhance logging and add support links
    Added support URLs and improved logging functionality.

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ import threading
 from rich.logging import RichHandler
 import logging
 from rich.padding import Padding
+from rich.highlighter import NullHighlighter
+import requests
 
 APP_VERSION='v0.9 pre'
 APP_VERSION_RELEASE=0
@@ -28,6 +30,9 @@ APP_DATE='2026/05'
 APP_UPDATECHK_ROOT='https://raw.githubusercontent.com/tromoSM/tromoSM-assets/main/repos/FLUXLAN/manifest.json'
 APP_LINK_LOOKUP='https://raw.githubusercontent.com/tromoSM/tromoSM-assets/main/root/info.json'
 APP_BUILD='beta'
+APP_SUPPORT='https://tromosm.ct.ws/?feedback=true&utm_source=normal_fluxlan_console'
+APP_SUPPORT_LTS_FEEDBACK="https://tromosm.github.io/tromoSM/t/?feedback=true&utm_source=lts_fluxlan_console"
+
 
 main=Flask(__name__,template_folder=os.path.join("templates"),static_folder=os.path.join("static"))
 
@@ -60,20 +65,23 @@ if DEVELOPER_MODE: #toggle this in production
  FlaskLog.level=logging.ERROR
 
 FluxLanLog=logging.getLogger(__name__)
-logging.basicConfig(handlers=[RichHandler(rich_tracebacks=True,show_level=False,show_path=False,show_time=False,markup=True)],format='%(message)s',level=logging.INFO)
+logging.basicConfig(handlers=[RichHandler(rich_tracebacks=True,show_level=False,show_path=False,show_time=False,markup=True,highlighter=NullHighlighter())],format='%(message)s',level=logging.INFO)
 
-def FluxLog(message,level='info',padding=1,CoverText=False):
+def FluxLog(message,level='info',padding=1,CoverText=False,KeyValues=False):
    colortable={'info':'bright_blue','error':'red',"high":'bright_red','debug':'magenta','warning':'yellow'}
-   if not CoverText:
+   if not CoverText and not KeyValues:
     message=f"[{colortable.get(level)}]|[/{colortable.get(level)}] {message}"
-   else:
+   elif not KeyValues:
     message=f"[{colortable.get(level)}]| {message}[/{colortable.get(level)}]"
+   if KeyValues:
+      colored=message.split(':',1)
+      message=f"[{colortable.get(level)}]| {colored[0]}:[/{colortable.get(level)}]{colored[1]}"
    Loglevel={"info":FluxLanLog.info,"error":FluxLanLog.error,'high':FluxLanLog.info,'debug':FluxLanLog.debug,'warning':FluxLanLog.warning}
    
    LogFunc=Loglevel.get(level)
    LogFunc(f'{" "*padding}{message}') 
 
-#STARTUP INFO
+#STARTUP 
 terminalsize=os.get_terminal_size()
 if(terminalsize.columns>=(107+len(APP_VERSION))):
  print(f"""
@@ -108,11 +116,28 @@ def refreshNetworkInfo():
         signal=int(line.split(':')[1].strip().replace('%',''))
         dBm=(signal//2)-100
         NetworkStrength={"signal":signal,"dBm":dBm}
+        FluxLog(f"Refreshing network info : signal {signal} dBm {dBm}dBm ",level='info',CoverText=True,KeyValues=True)
 refreshNetworkInfo()
+
+def linkLookup():
+   global APP_SUPPORT
+   try:
+      mainRes=requests.get(url=APP_SUPPORT,timeout=5)
+      if mainRes.status_code==200:
+         pass
+      else :
+         APP_SUPPORT=APP_SUPPORT_LTS_FEEDBACK
+         FluxLog(f'Main support site is returning {mainRes.status_code} : using fallback site',level='warning',KeyValues=True)
+   except requests.exceptions.ConnectionError:
+         APP_SUPPORT=APP_SUPPORT_LTS_FEEDBACK
+         FluxLog(f'Main support site is not responding : using fallback site',level='warning',KeyValues=True)
+
+linkLookup()
 
 # FIRST TIME
 if not os.path.exists(os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN')):
     os.makedirs(os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN'))
+    FluxLog('First time running : Creating media folders',KeyValues=True)
 
 if not os.path.exists(os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN','Captures')):
     os.makedirs(os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN',"Captures"))
@@ -131,11 +156,12 @@ try:
 except Exception:
    MainIP='127.0.0.1'
 finally:
+   FluxLog(f'Refreshing Local IP address {MainIP}')
    SOCKET.close()
 # GLOBAL UI FUNCc
 
-
 def joinev(user):
+    FluxLog(f'New camera connected : {user}',KeyValues=True)
     if user not in ALLUSERS:
      ALLUSERS.append(user)
     else:
@@ -146,10 +172,9 @@ def joinev(user):
 
 def leaveev(user):
     if user in ALLUSERS:
+        FluxLog(f'Camera disconnected : {user}',KeyValues=True)
         ALLUSERS.remove(user)
     S.emit('adminLEAVE',user)  
-    for i in ALLUSERS:
-       print(i)
     S.emit('CAMlist',ALLUSERS)  
 
 def AdminNotification(title='',body='',icon='__none__',timeout=4,level='info'):
@@ -167,8 +192,10 @@ def HostNotification(title,description,fallbackicon=0x40):
         maintk.show_toast(toast)
        except Exception as ex:
           ctypes.windll.user32.MessageBoxW(0,f"{title.upper()}\n{"￣"*(int(len(description)/2))}\n{description}","FluxLAN",fallbackicon)
+          FluxLog(f'Limited support - Windows version might be less than 10. Exeption : {ex}',level='warning',CoverText=True)
           #add to err log 10>win {ex}
-
+       finally:
+        FluxLog(f'Notification sent to host : {title}',KeyValues=True)
 
 def onerr(type,value,traceback):
    print(f'type : {type}\nvalue : {value}\ntraceback:{traceback}')
@@ -196,14 +223,12 @@ def mainroute(data):
 def ini(dih):
     MAINUSERSDI[request.sid]=dih['clieUSR']
     curuser=MAINUSERSDI.get(request.sid,request.sid)
-    print(f"│ User connected : {curuser}")
     joinev(curuser)
     print(len(MAINUSERSDI))
 
 @S.on('disconnect')
 def left():
     curUser=MAINUSERSDI.get(request.sid,request.sid)
-    print(f"│ User disconnected : {curUser}")
     leaveev(curUser)      
     MAINUSERSDI.pop(request.sid,None)
 
@@ -223,20 +248,23 @@ def revive(user):
 def ask(q):
    global RecordingRunning,MAINUSERSDI,ALLUSERS,APP_DATE,APP_UPDATECHK_ROOT,APP_VERSION,APP_VERSION_RELEASE,APP_LINK_LOOKUP,APP_BUILD,MainIP,MainPort
    if q=='suggStream':
-      S.emit('CAMlist',ALLUSERS)  
+      S.emit('CAMlist',ALLUSERS)
       return {'stream': len(MAINUSERSDI)}
    elif q=='ifRecRunning':
       return {'running': str(RecordingRunning)}
    elif q=='about':
       refreshNetworkInfo()
       return {'date':APP_DATE,'updatechk_root':APP_UPDATECHK_ROOT,'version':APP_VERSION,'version_release':str(APP_VERSION_RELEASE),'link_lookup':APP_LINK_LOOKUP,'build':APP_BUILD,"ip":MainIP,'port':MainPort,'protocol':Protocol,"strength":NetworkStrength}
-   
+
 @S.on('OpenFolder')
 def openF(fl):
     if sys.platform=='win32':
        if fl=='user/Pictures/app':
         os.startfile(os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN'))
-        
+        FluxLog(f'Opening {os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN')}')
+    else:
+       FluxLog(f'Couldnt execute function : FluxLAN do not fully support {sys.platform} yet. Report issue : {APP_SUPPORT}',level='error',KeyValues=True)        
+
 @main.route('/')
 def web():
  global Protocol
@@ -246,9 +274,11 @@ def web():
 @main.route('/dashboard')
 def admin():
    if(request.remote_addr!='127.0.0.1'):
+      FluxLog(f'Dashboard is fobidden to IPs other than 127.0.0.1 or localhost. Use localhost:{MainPort} or 127.0.0.1:{MainPort} instead.',level='warning',CoverText=True)
       abort(403)
       return 'sybau'
    return render_template('tromoSM-admin.html')
+
 @S.on('Pref')
 def mainfunc(pref):
         S.emit('JSPrefREC',pref)
@@ -263,7 +293,9 @@ def save(data):
     
     with open(f'{os.path.join(os.path.expanduser("~"),"Pictures","FluxLAN","Captures",f"Capture {str(datetimeX.date())}_{str(datetimeX.time()).replace(':','-')}.jpg")}',"wb") as im:
        im.write(base64.b64decode(base))
+    FluxLog(f'Image saved to : {os.path.join(os.path.expanduser("~"),"Pictures","FluxLAN","Captures",f"Capture {str(datetimeX.date())}_{str(datetimeX.time()).replace(':','-')}.jpg")}',KeyValues=True)
     return {"file":str(f"Capture {str(datetimeX.date())}_{str(datetimeX.time()).replace(':','-')}"),'dir':"Captures"}
+
 @S.on("OpenSelected")
 def openS(data):
  path=data.get('file')
@@ -272,30 +304,34 @@ def openS(data):
    try:
     subprocess.Popen(f'explorer /select,{os.path.normpath(os.path.join(os.path.expanduser("~"),"Pictures","FluxLAN",folder,f"{path}.jpg"))}') 
    except Exception as err:
-    print(err)
+    FluxLog(f'Couldnt execute function : {err}',level='error',KeyValues=True)
  else: 
    os.startfile(os.path.join(os.path.expanduser("~"),"Pictures","FluxLAN",folder,f"{path}.jpg"))
+   FluxLog(f'Some of the features might not work. FluxLan doesnt fully support {sys.platform} yet. Report issue : {APP_SUPPORT}',level='warning',KeyValues=True)        
 
 @S.on('OpenDir')
 def openD(path):
     os.startfile(os.path.join(os.path.expanduser("~"),"Pictures","FluxLAN",path))
+    FluxLog(f'Opening FluxLan media folder')
 
 @S.on('Record')
 def record(data):
     global RecordingRunning,RECORDED_DATA,CurrentRecordStream,LASTrecStamp
     if data.get('command')=='start':
        if not RecordingRunning:
-            print('Start rec')
+            FluxLog('Starting recording')
             CurrentRecordStream=data.get('stream')
             RecordingRunning=True
             LASTrecStamp=datetime.now()
 
        else: 
          RECORDED_DATA.clear()
+         FluxLog('A recording is still running : Clearing current recording to start new recording',level='error',KeyValues=True)
+
     elif data.get('command')=='stop':
         if RecordingRunning: 
            RecordingRunning=False
-           print('stop')
+           FluxLog('Stopping recording')
            AdminNotification(title='Processing recorded video',body='Recorded video is currently processing.',timeout=3)
            tempbinaryrecs=bytearray()
            for frame in RECORDED_DATA:
@@ -309,6 +345,7 @@ def record(data):
 
 @S.on('hostpref') #dont use (to v1+)
 def pref(com):
+    FluxLog('System is using a beta function',level='error',CoverText=True)
     prefr=com.get("pref")
     with open(os.path.normpath(os.path.join(platformdirs.user_data_dir(appname='FluxLAN',appauthor='tromoSM'),'preferences.json')),'r') as pref:
      saved=json.load(pref)
@@ -323,19 +360,23 @@ def pref(com):
 @S.on('BatteryChange')
 def battery(data):
    S.emit('adminBatteryChange',data) 
+   FluxLog(f"{data.get('user')} reported a battery change with the status : {data.get('status')}",KeyValues=True)
 
 @S.on('ClearAll')
 def clear(e):
    S.emit('ClientClear','')
+   FluxLog('Recieved signal to clear localstorage on all connected devices')
 
 @S.on('ClearedAll')
 def cleared(user):
+   FluxLog(f"{user}'s localstorage was cleared.")
    S.emit('AdminCleared','')
    AdminNotification(title=f"{user}'s localstorage is being cleared",body=f"{user}'s saved preferences and usernames are being cleared",timeout=5,level='_info_')
 
 @S.on('CloseSelf')
 def close(ver):
    if(ver=='verified'):
+    FluxLog('Closing FluxLAN',level='high',CoverText=True)
     AdminNotification(title='FluxLAN is closing',body='FluxLAN will be closed in a minute',timeout="never")
     S.emit('closing','normal')
     os.kill(os.getpid(),signal.SIGINT)
@@ -361,6 +402,7 @@ def MotionDetect():
                   #add hostnotiff if pref notifications secr
                   with open(f'{os.path.join(os.path.expanduser("~"),'Pictures','FluxLAN',"Motion detected",f"Capture {str(datetimeX.date())}_{str(datetimeX.time()).replace(':','-')}.jpg")}',"wb") as im:
                    im.write(base64.b64decode(base))        
+                   FluxLog(f'Motion detected in {LASTFrame.get('camname')} at {datetimeX.time()}',CoverText=True)
                    AdminNotification(title=f'Motion detected in {LASTFrame.get('camname')}',body=f"""Motion detected in {LASTFrame.get('camname')}. Image captured <span onclick="openDir('Motion detected')">Captures/</span><span button="open" onclick="openS('Capture {str(datetimeX.date())}_{str(datetimeX.time()).replace(':','-')}','Motion detected')">open</span>""",timeout='5')
             MotionFrameLS=eachfr
 
@@ -368,8 +410,10 @@ def MotionDetect():
 def command(data):
    global MotionDetecting,MotionFrameSkip,MotionFrameLS
    if data.get('command')=='start':
+    FluxLog('Starting Motion detection')
     MotionDetecting=True
    elif data.get('command')=='stop':
+    FluxLog('Stopping Motion detection')
     MotionDetecting=False
     MotionFrameLS=None
     MotionFrameSkip=0
@@ -382,6 +426,8 @@ def refresh(info):
 
 @S.on('exportdata')
 def export(jsond):
+   print('\n')
+   FluxLog('Exporting data')
    #stackoverflow/a/14119223
    datetimeX=datetime.now()
    additionalinfo={
@@ -397,38 +443,50 @@ def export(jsond):
       },
       "SessionData":{"ALLUSERS":ALLUSERS,"IP":MainIP,"Port":MainPort}
    }
-
+   FluxLog('Writing system info to export')
    jsondata=json.loads(jsond)
    jsondata.update(additionalinfo)
    def saveExport():
+    FluxLog('Opening file dialog to choose saving path')
     root.lift()
     root.attributes('-topmost',True)
     accepted=[('FluxLan backup files','*.fluxlan*')]
     saved=filedialog.asksaveasfile(filetypes=accepted,defaultextension='.fluxlan',title='Save export/backup file',initialfile=f"FluxLAN backup {datetimeX.date()}")
     root.update()
     root.withdraw()
+    FluxLog('Closing file dialog')
     if saved:
+       FluxLog('Saving export file')
        json.dump(jsondata,saved)
        saved.close()
+   print('\n')
    tk_q.put(saveExport)
 
 if not DEVELOPER_MODE:
+ FluxLog('Opening dashboard')
  webbrowser.open(f'https://localhost:{MainPort}/dashboard')
 
 @S.on('importdata')
+   
 def importpref():
+ print('\n')
+ FluxLog('Importing backup/export file')
  def openImport():
+  FluxLog('Opening file dialog to choose what file import from')
   root.lift()
   root.attributes('-topmost',True)
   accepted=[('FluxLan backup files','*.fluxlan*')]
   imported=filedialog.askopenfilename(filetypes=accepted,title='Import export/backup file',defaultextension='.fluxlan')
   root.update()
   root.withdraw()
+  FluxLog('Closing file dialog')
   if imported:
+     FluxLog('Reading exported data')
      with open(imported,'r') as backup:
         backupd=json.load(backup)
         S.emit('recieveImport',backupd)
-
+        FluxLog('Applying backup to current system')
+ print('\n')
  tk_q.put(openImport) 
 
 sys.excepthook=onerr   
